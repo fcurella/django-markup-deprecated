b19769dc5606b399ec94ab5b01f30910ec926ee0
Flavio Curella <flavio.curella@gmail.com>
2014-01-13 11:21:59 -0600

    Bump version: 0.0.2 → 0.0.3

--- markup_deprecated/__init__.py
+++ markup_deprecated/__init__.py
@@ -1,2 +1,2 @@
-_version = "0.0.2"
+_version = "0.0.3"
 __version__ = tuple(map(int, _version.split('.')))
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 
 def read(fname):
